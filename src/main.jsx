be61b4b0515b0442dce1e25dd599114cfb2b03d6
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2026-02-17 16:55:58 +0600

    base name add

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import DetailPage from './DetailPage/DetailPage.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <BrowserRouter>
+    <BrowserRouter basename="/istore">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/detail/:id" element={<DetailPage />} />
